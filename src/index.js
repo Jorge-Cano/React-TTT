2f340481abc034e026a5828fd6f95f05de946f53
Jorge Cano <jacinaustin@gmail.com>
2018-02-06 16:24:09 -0600

    set state for next player

--- src/index.js
+++ src/index.js
@@ -34,8 +34,11 @@ class Board extends React.Component {
   handleClick(i) {
     const squares = this.state.squares.slice();
     squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({squares: squares});
-  }
+    this.setState({
+      squares: squares,
+    xIsNext: !this.state.xIsNext,
+  });
+}
 
   renderSquare(i) {
     return (
@@ -47,7 +50,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
