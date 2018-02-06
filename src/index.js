183b08135a2384fa8d2906be7d33d70614d95150
Jorge Cano <jacinaustin@gmail.com>
2018-02-06 16:09:42 -0600

    multiple state changes and constructor has been omitted, no longer needed

--- src/index.js
+++ src/index.js
@@ -4,17 +4,10 @@ import './index.css';
 
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
-  render() {
+  render(){
     return (
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
@@ -28,9 +21,15 @@ class Board extends React.Component {
     };
   }
 
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return
-      (<Square
+    return (
+      <Square
         value={this.state.squares[i]}
         onClick={() => this.handleClick(i)}
       />
